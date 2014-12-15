4cf2c081b9f56eb5db2e4785f834ab0ec52a3eec
LukaszRabiec <259049@fizyka.umk.pl>
2014-12-15 21:55:57 +0100

    Komyt

--- WindowsFormsApplication66/Matrix_data.cs
+++ WindowsFormsApplication66/Matrix_data.cs
@@ -16,7 +16,7 @@ namespace LED_Matrix
     {
 
 
-        List<Matrix_sign> matrix_list = new List<Matrix_sign> //Hehe
+        List<Matrix_sign> matrix_list = new List<Matrix_sign> //Heheheheh
             {
                 new Matrix_sign() { sign='a', msign=new Int16[,] { {0,0,0,0,0},
                                                                    {0,0,0,0,0},
